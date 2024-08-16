4c933a81cdd10434465948165b141fbf8c1064bb
Leonardo Straccali <leonardo.straccali@gmail.com>
2024-08-16 14:41:10 +0200

    feat: operas

---
+++ src/data/operasData.ts
@@ -0,0 +1,40 @@
+import TISECAT from "@operas/TISECAT.webp";
+import NEWTREE from "@operas/NEWTREE.webp";
+import light from "@operas/light.webp";
+import Euler from "@operas/Euler.webp";
+import Godel from "@operas/Godel.webp";
+import DALWAVE from "@operas/DALWAVE.webp";
+import GHOSSIAN from "@operas/GHOSSIAN.webp";
+
+const operas = [
+    {
+        src: TISECAT.src,
+        title: "TISECAT",
+    },
+    {
+        src: NEWTREE.src,
+        title: "NEWTREE",
+    },
+    {
+        src: light.src,
+        title: "light",
+    },
+    {
+        src: Euler.src,
+        title: "Euler",
+    },
+    {
+        src: Godel.src,
+        title: "Godel",
+    },
+    {
+        src: DALWAVE.src,
+        title: "DALWAVE",
+    },
+    {
+        src: GHOSSIAN.src,
+        title: "GHOSSIAN",
+    },
+]
+
+export default operas;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'caveat': ['Caveat'],
+				'flower' : ['Indie Flower'],
+				'gloria': ['Gloria Hallelujah'],
+			},
+		},
 	},
 	plugins: [],
 }
