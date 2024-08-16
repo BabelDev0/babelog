59f6d7f1f22512cd52ed1d83b66a00e50933c37b
Leonardo Straccali <leonardo.straccali@gmail.com>
2024-08-16 14:46:46 +0200

    refactor: change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://BabelDev0.github.io',
-  base: '/',
+  base: '/babelog',
 });
